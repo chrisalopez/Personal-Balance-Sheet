babbeca34c357b45f66a88f07d1f1b2d4b67ea9d
Christopher Lopez <chrisa_lopez@yahoo.com>
2016-07-22 14:37:36 -0700

    changed from light to progresql

--- personal_balance_sheet/db/schema.rb
+++ personal_balance_sheet/db/schema.rb
@@ -11,9 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160722190110) do
+ActiveRecord::Schema.define(version: 20160722191855) do
 
-  create_table "models", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -28,7 +31,7 @@ ActiveRecord::Schema.define(version: 20160722190110) do
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "models", ["email"], name: "index_models_on_email", unique: true
-  add_index "models", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
