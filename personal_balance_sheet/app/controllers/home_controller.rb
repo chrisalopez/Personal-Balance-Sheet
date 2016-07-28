600598869adaf1b421b60c310a8c0e3444ae0e0e
Christopher Lopez <chrisa_lopez@yahoo.com>
2016-07-28 10:34:13 -0700

    Linked portfolio page with Data Information

--- personal_balance_sheet/app/controllers/home_controller.rb
+++ personal_balance_sheet/app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @datainputs = DataInput.where(user_id: current_user.id)
   end
 
   def form
@@ -9,10 +10,13 @@ class HomeController < ApplicationController
   end
 
   def submit
-
+    d = DataInput.new(data_input_params)
+    d.user_id = current_user.id
+    d.save
+    redirect_to '/home/index', alert: "Watch it, mister!"
   end
 
-  def post
-    # redirect_to '/home/index', alert: "Watch it, mister!"
-  end
+  def data_input_params
+      params.require(:data_input).permit(:date, :income, :rent, :loans, :insurance, :utilities, :entertainment, :food, :misc, :balance, :comment)
+    end
 end
